3eddace838114e8faa387ca0f393a920349d66f7
Pablo Guinard <guinardpablo@gmail.com>
2022-06-15 14:32:51 +0200

    bug fix popup

--- PHP/js/popups.js
+++ PHP/js/popups.js
@@ -2,9 +2,17 @@ const textes = document.getElementsByClassName('divRadioTexte')
 const images = document.getElementsByClassName('divRadioImage')
 const videos = document.getElementsByClassName('divRadioVideo')
 
-const inputTextes = document.getElementsByClassName('divInputTexte')
+const tmp = document.getElementsByClassName('divInputTexte')
 const inputImages = document.getElementsByClassName('divInputImage')
 const inputVideos = document.getElementsByClassName('divInputVideo')
+
+let inputTextes = []
+for(let cpt = 2; cpt < tmp.length; cpt++){
+    inputTextes.push(tmp[cpt])
+}
+console.log(inputTextes.length);
+console.log(inputImages.length);
+console.log(inputVideos.length);
 
 const radioImage = document.querySelectorAll('#imageRadio')
 const radioTexte = document.querySelectorAll('#texteRadio')
